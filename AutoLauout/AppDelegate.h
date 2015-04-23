086f94e2df3be6cecf45cc550e4e408323735a70
iampaul83 <iampaul783@gmail.com>
2015-04-23 13:38:55 +0800

    Initial Commit

---
+++ AutoLauout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLauout
+//
+//  Created by Paul on 4/23/15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
